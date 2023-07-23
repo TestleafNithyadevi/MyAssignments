3bfaf6b93e8bc920b66b36f833bb8e2560397b05
TestleafNithyadevi <140292372+TestleafNithyadevi@users.noreply.github.com>
2023-07-23 08:46:15 +0530

    Day1

---
+++ day1/Datatypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		int Age=32;
+		float Height=5.1f;
+		double Salary=33000.55;
+		char Initial='S';
+		String Name="Nithyadevi";
+		long Mobileno=9986702002l;
+		boolean Isworking=true;
+		
+		System.out.println("My Age is "+Age);
+		System.out.println("My height is "+Height);
+		System.out.println("My salary is "+Salary);
+		System.out.println("My name is "+Initial+"."+Name);
+		System.out.println("My mobileno is "+Mobileno);
+		System.out.println("Working Women "+Isworking);
+	}
+}
---
+++ day1/OddorEven.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class OddorEven {
+
+public static void main(String[] args) {
+	
+	int number = 15;
+	
+	System.out.println("The Number is " +number);
+	
+	if(number%2==0) {
+		
+		System.out.println("The Number is even");
+		
+		}
+	else {
+		System.out.println("The Number is odd");
+}
+		
+	}
+}
